cf84c49a9eff04daa1e31df7da5d5be3de8e0105
kirsbar <barak521@newschool.edu>
2024-09-23 15:34:52 -0400

    committting

---
+++ unit-1/lesson-5/unit1projectidea.py
@@ -0,0 +1,12 @@
+#### directory with images of punch cards called dir 1
+#### directory with images of pages from novels dir 2
+#### directory with images of random objects in random places dir 3
+#### code that will randomly call from dir 1
+#### code that will randomly call from dir 2
+#### code that will randomly call from dir 3
+#### code that will overlay and blend all three images randomly called
+####### remember to think about opacity too
+#### outcome: randomly generates image that shows words circled in a sentence that also has a sticker type thing somewhere
+##### maybe add gradient?? or play with colors for filter generated image
+##### play with randomly choosing pixel in generated image to produce a highlighter or colored part
+##### try to make it 800
